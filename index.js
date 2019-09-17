5da68693ad3447c5cca5b6d2319534584b912d33
daniloimparato <imparatodanilo@gmail.com>
2019-09-17 19:10:51 -0300

    attempt to move multiple area-selected nodes

--- index.js
+++ index.js
@@ -68,6 +68,39 @@ fetch('patricia.json')
         layout: layout
       });
 
+    var events = Viva.Graph.webglInputEvents(graphics, graph);
+
+    var selectedNodes = [];
+
+    var initialPosition = {x: 0, y:0};
+    var movePosition = {x: 0, y:0};
+
+    events.mouseEnter(function (node) {
+      console.log('Mouse entered node: ', node);
+    }).mouseLeave(function (node) {
+      console.log('Mouse left node: ', node);
+    }).dblClick(function (node) {
+      console.log('Double click on node: ', node);
+    }).click(function (node) {
+      console.log('Single click on node: ', node);
+    }).mouseDown(function (node) {
+      console.log('Mouse down on node: ', node);
+      initialPosition = layout.getNodePosition(node.id);
+    }).mouseMove(function (node) {
+      console.log('Mouse move on node: ', node);
+      graph.forEachNode(function(eachNode){
+        if(selectedNodes.map(selNodes=>{selNodes.id}).includes(eachNode.id)) {
+          var currPos = layout.getNodePosition(eachNode.id);
+          var diffX = layout.getNodePosition(node.id).x - initialPosition.x;
+          var diffY = layout.getNodePosition(node.id).y - initialPosition.y;
+          layout.setNodePosition(eachNode, currPos.x + diffX, currPos.y + diffY);
+        }
+      });
+      // movePosition = layout.getNodePosition(node.id);
+    }).mouseUp(function (node) {
+      console.log('Mouse up on node: ', node);
+    });
+
     renderer.run();
 
     //////////////////////////////////
@@ -104,13 +137,13 @@ fetch('patricia.json')
     // area selection
     //////////////////////////////////
     var multiSelectOverlay;
-  
-    document.addEventListener('keydown', function(e) {
+
+    document.addEventListener('keydown', function (e) {
       if (e.which === 16 && !multiSelectOverlay) { // shift key
-        multiSelectOverlay = startMultiSelect(graph, renderer, layout);
+        multiSelectOverlay = startMultiSelect(graph, renderer, layout, selectedNodes);
       }
     });
-    document.addEventListener('keyup', function(e) {
+    document.addEventListener('keyup', function (e) {
       if (e.which === 16 && multiSelectOverlay) {
         multiSelectOverlay.destroy();
         multiSelectOverlay = null;
@@ -120,120 +153,128 @@ fetch('patricia.json')
   });
 
 
-  function startMultiSelect(graph, renderer, layout) {
-    var graphics = renderer.getGraphics();
-    var domOverlay = document.querySelector('.graph-overlay');
-    var overlay = createOverlay(domOverlay);
-    overlay.onAreaSelected(handleAreaSelected);
-  
-    return overlay;
-  
-    function handleAreaSelected(area) {
-      // For the sake of this demo we are using silly O(n) implementation.
-      // Could be improved with spatial indexing if required.
-      var topLeft = graphics.transformClientToGraphCoordinates({
-        x: area.x,
-        y: area.y
-      });
-  
-      var bottomRight = graphics.transformClientToGraphCoordinates({
-        x: area.x + area.width,
-        y: area.y + area.height
-      });
-  
-      graph.forEachNode(higlightIfInside);
-      renderer.rerender();
-  
-      return;
-  
-      function higlightIfInside(node) {
-        var nodeUI = graphics.getNodeUI(node.id);
-        if (isInside(node.id, topLeft, bottomRight)) {
-          nodeUI.color = 0xFFA500ff;
-          // nodeUI.size = 20;
-        } else {
-          nodeUI.color = 0x000000ff;
-          // nodeUI.size = 10;
-        }
+function startMultiSelect(graph, renderer, layout, selectedNodes) {
+  var graphics = renderer.getGraphics();
+  var domOverlay = document.querySelector('.graph-overlay');
+  var overlay = createOverlay(domOverlay);
+  overlay.onAreaSelected(handleAreaSelected);
+
+  return overlay;
+
+  function handleAreaSelected(area) {
+    // For the sake of this demo we are using silly O(n) implementation.
+    // Could be improved with spatial indexing if required.
+    var topLeft = graphics.transformClientToGraphCoordinates({
+      x: area.x,
+      y: area.y
+    });
+
+    var bottomRight = graphics.transformClientToGraphCoordinates({
+      x: area.x + area.width,
+      y: area.y + area.height
+    });
+    
+    selectedNodes = [];
+
+    graph.forEachNode(higlightIfInside);
+
+    console.log(selectedNodes);
+
+    renderer.rerender();
+
+    return;
+
+    function higlightIfInside(node) {
+      var nodeUI = graphics.getNodeUI(node.id);
+      if (isInside(node.id, topLeft, bottomRight)) {
+        console.log(node);
+        nodeUI.color = 0xFFA500ff;
+
+        selectedNodes.push(node);
+
+        // nodeUI.size = 20;
+      } else {
+        nodeUI.color = 0x000000ff;
+        // nodeUI.size = 10;
       }
-  
-      function isInside(nodeId, topLeft, bottomRight) {
-        var nodePos = layout.getNodePosition(nodeId);
-        return (topLeft.x < nodePos.x && nodePos.x < bottomRight.x &&
-          topLeft.y < nodePos.y && nodePos.y < bottomRight.y);
-      }
+    }
+
+    function isInside(nodeId, topLeft, bottomRight) {
+      var nodePos = layout.getNodePosition(nodeId);
+      return (topLeft.x < nodePos.x && nodePos.x < bottomRight.x &&
+        topLeft.y < nodePos.y && nodePos.y < bottomRight.y);
     }
   }
-  
-  function createOverlay(overlayDom) {
-    var selectionClasName = 'graph-selection-indicator';
-    var selectionIndicator = overlayDom.querySelector('.' + selectionClasName);
-    if (!selectionIndicator) {
-      selectionIndicator = document.createElement('div');
-      selectionIndicator.className = selectionClasName;
-      overlayDom.appendChild(selectionIndicator);
-    }
-  
-    var notify = [];
-    var dragndrop = Viva.Graph.Utils.dragndrop(overlayDom);
-    var selectedArea = {
-      x: 0,
-      y: 0,
-      width: 0,
-      height: 0
-    };
-    var startX = 0;
-    var startY = 0;
-  
-    dragndrop.onStart(function(e) {
-      startX = selectedArea.x = e.clientX;
-      startY = selectedArea.y = e.clientY;
-      selectedArea.width = selectedArea.height = 0;
-  
-      updateSelectedAreaIndicator();
-      selectionIndicator.style.display = 'block';
-    });
-  
-    dragndrop.onDrag(function(e) {
-      recalculateSelectedArea(e);
-      updateSelectedAreaIndicator();
-      notifyAreaSelected();
-    });
-  
-    dragndrop.onStop(function() {
-      selectionIndicator.style.display = 'none';
-    });
-  
-    overlayDom.style.display = 'block';
-  
-    return {
-      onAreaSelected: function(cb) {
-        notify.push(cb);
-      },
-      destroy: function () {
-        overlayDom.style.display = 'none';
-        dragndrop.release();
-      }
-    };
-  
-    function notifyAreaSelected() {
-      notify.forEach(function(cb) {
-        cb(selectedArea);
-      });
-    }
-  
-    function recalculateSelectedArea(e) {
-      selectedArea.width = Math.abs(e.clientX - startX);
-      selectedArea.height = Math.abs(e.clientY - startY);
-      selectedArea.x = Math.min(e.clientX, startX);
-      selectedArea.y = Math.min(e.clientY, startY);
-    }
-  
-    function updateSelectedAreaIndicator() {
-      selectionIndicator.style.left = selectedArea.x + 'px';
-      selectionIndicator.style.top = selectedArea.y + 'px';
-      selectionIndicator.style.width = selectedArea.width + 'px';
-      selectionIndicator.style.height = selectedArea.height + 'px';
-    }
+}
+
+function createOverlay(overlayDom) {
+  var selectionClasName = 'graph-selection-indicator';
+  var selectionIndicator = overlayDom.querySelector('.' + selectionClasName);
+  if (!selectionIndicator) {
+    selectionIndicator = document.createElement('div');
+    selectionIndicator.className = selectionClasName;
+    overlayDom.appendChild(selectionIndicator);
   }
-  
+
+  var notify = [];
+  var dragndrop = Viva.Graph.Utils.dragndrop(overlayDom);
+  var selectedArea = {
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0
+  };
+  var startX = 0;
+  var startY = 0;
+
+  dragndrop.onStart(function (e) {
+    startX = selectedArea.x = e.clientX;
+    startY = selectedArea.y = e.clientY;
+    selectedArea.width = selectedArea.height = 0;
+
+    updateSelectedAreaIndicator();
+    selectionIndicator.style.display = 'block';
+  });
+
+  dragndrop.onDrag(function (e) {
+    recalculateSelectedArea(e);
+    updateSelectedAreaIndicator();
+    notifyAreaSelected();
+  });
+
+  dragndrop.onStop(function () {
+    selectionIndicator.style.display = 'none';
+  });
+
+  overlayDom.style.display = 'block';
+
+  return {
+    onAreaSelected: function (cb) {
+      notify.push(cb);
+    },
+    destroy: function () {
+      overlayDom.style.display = 'none';
+      dragndrop.release();
+    }
+  };
+
+  function notifyAreaSelected() {
+    notify.forEach(function (cb) {
+      cb(selectedArea);
+    });
+  }
+
+  function recalculateSelectedArea(e) {
+    selectedArea.width = Math.abs(e.clientX - startX);
+    selectedArea.height = Math.abs(e.clientY - startY);
+    selectedArea.x = Math.min(e.clientX, startX);
+    selectedArea.y = Math.min(e.clientY, startY);
+  }
+
+  function updateSelectedAreaIndicator() {
+    selectionIndicator.style.left = selectedArea.x + 'px';
+    selectionIndicator.style.top = selectedArea.y + 'px';
+    selectionIndicator.style.width = selectedArea.width + 'px';
+    selectionIndicator.style.height = selectedArea.height + 'px';
+  }
+}
